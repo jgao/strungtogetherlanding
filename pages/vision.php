57624daed7595898af65cf87ca97624d3910a64c
Jack Gao <jackgao2006@gmail.com>
2013-02-11 00:45:31 -0800

    minor name change

---
+++ pages/vision.php
@@ -0,0 +1,62 @@
+<div class="heading">
+  Know more. Recommend more. Sell more. No hassle.
+</div>
+<div class="subheading">
+  VIPER is a personalized recommendation engine that is truly <b>personal</b>.<br />
+  It's as simple as 1, 2, 3.
+</div>
+<div class="tourstep">
+  <div class="tournumber">STEP 1:</div>
+  <div class="toursteptext">Install VIPER in minutes on <i>any</i> platform.</div>
+  <div class="tourbody">
+    <img id="integration" src="<?php echo IMAGE_BASE.'integration.jpg'; ?>" alt="integration" />
+    <p>VIPER works straight out-of-the-box. Integration takes literally 5 minutes.
+       We provide you with a dead-simple API composing of 4 methods (see our developer section).
+    </p>
+    <p>Installation is non-intrusive leaving your business' workflow remains unchanged.
+    </p>
+    <p>VIPER requires no maintenance. We also offer the flexibity to install VIPER for you. We want you to simply install VIPER, then forget about it.
+    </p>
+  </div>
+</div>
+<div class="tourstep">
+  <div class="tournumber">STEP 2:</div>
+  <div class="toursteptext">Gather data about your customers with VIPER - <i>automatically.</i></div>
+  <div class="tourbody">
+    <img id="clustering" src="<?php echo IMAGE_BASE.'clustering.png'; ?>" alt="clustering" />
+    <p>VIPER tracks actions your customers take in your store.
+    </p>
+    <p>Actions such as looking at a product, or purchasing a product will be used to cluster and
+       classify each individual user - allowing you to make personalized recommendations.
+    </p>
+    <p>VIPER pools data together from stores in similar industries.
+       This means your recommendations are based on 10x or even 100x the amount of data you have alone.
+    </p>
+    <p>All data is stored and processed by our servers saving you the trouble of building expensive infrasturcture.
+    </p>
+  </div>
+</div>
+<div class="tourstep">
+  <div class="tournumber">STEP 3:</div>
+  <div class="toursteptext">Give truly <i>personal</i> recommendations, powered by VIPER.</div>
+  <div class="tourbody">
+    <img id="recommendation" src="<?php echo IMAGE_BASE.'recommendation.png'; ?>" alt="recommendation" />
+    <p>VIPER's recommendation algorithm is an industry first.
+       Unlike similar services, we use users as the common denominator, instead of products.
+    </p>
+    <p>A user's recommendations is a function of other users' purchasing history.
+       Although analyzing product characteristics can yield decent results, VIPER's focus on user behaviour make the results much more accurate and personal.
+    </p>
+    <p>
+       VIPER isn't just your conventional "similar to this product" generator.
+       We look deeply into users, cluster them based on purchasing history,
+       and with this profile, we give recommendations that are truly personal.
+    </p>
+  </div>
+</div>
+<div class="subheading">
+  <p>That's it. VIPER is simple to install, yet powerful. No maintenance, no additional servers, no hassle.
+     Signup today.
+  </p>
+</div>
+
